92f5117edbb19abb9c274cd351c6d2359bdf0fdf
davidrey <david_sardy@hotmail.com>
2025-02-07 13:34:24 +0100

    Exercise 30 Resolto

--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -2,11 +2,21 @@ package com.campusdual.classroom;
 
 public class Exercise30 {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
+    public static int divisionWithCustomException (int dividend, int divisor) throws DivisionByZeroException{
+        if (divisor == 0){
+            throw new DivisionByZeroException("El divisor no puede ser 0");
+        }
+        return dividend / divisor;
 
     }
 
     public static void main(String[] args) {
+        try{
+            double result = Exercise30.divisionWithCustomException(20, 0);
+            System.out.println("Resultado: " + result);
+        }catch (DivisionByZeroException e){
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 }
